ffa87e1abfebefb6fabb75f8f1ab83f3543908d9
nisargrathod <165247042+nisargrathod@users.noreply.github.com>
2024-04-29 20:28:56 +0530

    Update main.py

--- main.py
+++ main.py
@@ -254,7 +254,7 @@ time_spend_company
         if page == "Prediction":
             with header:
                 st.header("🏦 Prediction Model 👨‍💼")
-                prediction_option = option_menu(menu_title=None, options=["One Value"], #'From Fil'
+                prediction_option = option_menu(menu_title=None, options=["Predict Employee Expected To STAY/LEAVE"], #'From File'
                                                 icons=[" "], menu_icon="cast", default_index=0,
                                                 orientation="horizontal", styles=sub_options_style)
 
