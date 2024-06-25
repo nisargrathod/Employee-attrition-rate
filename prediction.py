879d66b91f679e645ecbff2388160d004d42ba8b
nisargrathod <165247042+nisargrathod@users.noreply.github.com>
2024-06-25 22:52:33 +0530

    Update prediction.py

--- prediction.py
+++ prediction.py
@@ -3,41 +3,31 @@ import pandas as pd
 import numpy as np
 import plotly.express as px
 
-
 import streamlit as st
 import warnings
 
-
-def creat_matrix_score_cards(card_image="", card_title="Card Title", card_value=None, percent=False):
-    st.image(card_image,
-             caption="", width=70)
-
-    st.subheader(
-        card_title)
-
+# Function to create matrix score cards
+def create_matrix_score_cards(card_image="", card_title="Card Title", card_value=None, percent=False):
+    st.image(card_image, caption="", width=70)
+    st.subheader(card_title)
+    
     if percent:
-        st.subheader(
-            f"{card_value}%")
-
+        st.subheader(f"{card_value}%")
     else:
-        st.subheader(
-            f"{card_value}")
+        st.subheader(f"{card_value}")
 
-
+# Function to create a comparison DataFrame
 def create_comparison_df(y_actual, y_pred):
-    predected_df = pd.DataFrame()
-    predected_df["Actual Spent Values"] = y_actual
-    predected_df.reset_index(
-        drop=True, inplace=True)
-    predected_df["Predicted Spent Value"] = y_pred
+    predicted_df = pd.DataFrame()
+    predicted_df["Actual Spent Values"] = y_actual
+    predicted_df.reset_index(drop=True, inplace=True)
+    predicted_df["Predicted Spent Values"] = y_pred
+    return predicted_df
 
-    return predected_df
-
-
+# Function to create a confusion matrix plot
 def create_confusion_plot(cm):
     fig = px.imshow(cm, aspect=True, text_auto="0.0f", template="plotly_dark",
-                    color_continuous_scale="greens", x=["Stay", "Left"], y=["Saty", "Left"], height=550)
-
+                    color_continuous_scale="greens", x=["Stay", "Left"], y=["Stay", "Left"], height=550)
     fig.update_traces(
         textfont={
             "size": 15,
